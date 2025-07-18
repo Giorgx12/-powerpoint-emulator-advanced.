833a9e400f6da4e584e37fd81ce5aff37bb79792
Giorgx12 <giorgiobridarolli@gmail.com>
2025-07-18 14:22:08 +0200

    Update urls.py

--- powerapp/urls.py
+++ powerapp/urls.py
@@ -1,1 +1,22 @@
+from django.shrotcuts import render
+from django.http import JsonResponse
+import json
 
+# Dizionario di esempio per le presentazioni
+presentations = {}
+
+def index(request):
+    return render(request, 'powerapp/index.html')
+
+def save_presentation(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        presentation_id = data.get('id', 'default')
+        presentations[presentation_id] = data
+        return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'error'})
+
+def load_presentation(request):
+    presentation_id = request.GET.get('id', 'default')
+    data = presentations.get(presentation_id, {})
+    return JsonResponse(data)
